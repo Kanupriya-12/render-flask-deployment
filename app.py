407fd2443c3a96e18a0e195f801365ac2a61b471
Kanupriya12 <kanupriyamalhotra12@gmail.com>
2025-03-21 18:23:55 +0800

    Next update

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return "<h1>Kanupriya's Flask App</h1>"
 
+@app.route('/get_name', methods=['GET'])
+def get_name():
+    name = request.args.get('name')
+    if not name:
+        return jsonify({"error": "Missing 'name' parameter"}), 400
+    
+    return jsonify({"message": f"{name}'s Flask app"})
+
 if __name__== "__main__":
     app.run()
